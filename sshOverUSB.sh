436422a1fb69db2555c8686977533493211a231a
ralphandalis <ralph-nicole.nun.andalis@hp.com>
2016-01-26 22:35:46 +0800

    my ssh over usb script, for my convenience

--- sshOverUSB.sh
+++ sshOverUSB.sh
@@ -7,10 +7,12 @@
 # directory of the iphonessh repo where it is cloned
 dir="/Users/ralphnicoleandalis/iphonessh/python-client"
 cd "$dir"
+# this code is problematic, causes log when run
 # open -a Terminal run_script.sh
 echo -e "Type the following command in the open terminal: "
 echo -e "COMMAND: 'ssh root@localhost -p 2222'"
 open -a Terminal $dir
 
- # && `bash run_script.sh`
+# this code is problematic
+# && `bash run_script.sh`
 python tcprelay.py -t 22:2222
